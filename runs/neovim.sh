53e2346f4fa14628d8e3916950f64717e4850595
PoeAudits <161568076+PoeAudits@users.noreply.github.com>
2025-02-19 16:45:51 -0800

    Update neovim.sh

--- runs/neovim.sh
+++ runs/neovim.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-git clone -b v0.11.0 https://github.com/neovim/neovim.git $HOME/Tom/neovim
+git clone -b v0.10.0 https://github.com/neovim/neovim.git $HOME/lib/neovim
 sudo apt install cmake gettext lua5.1 liblua5.1-0-dev
 
-cd $HOME/Tom/neovim
+cd $HOME/lib/neovim
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
